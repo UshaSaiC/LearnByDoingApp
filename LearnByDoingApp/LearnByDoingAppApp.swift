b7aae6340b37988e00a4611c1778df1f56f63811
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-22 00:27:30 +0530

    Initial Commit

---
+++ LearnByDoingApp/LearnByDoingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingAppApp.swift
+//  LearnByDoingApp
+//
+//  Created by Usha Sai Chintha on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
