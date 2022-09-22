365fc20074fc98c79ef770e8d3ec2190c1a1be56
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-22 09:27:21 +0530

    Added animations and haptic feedback to card view

--- LearnByDoingApp/CardView.swift
+++ LearnByDoingApp/CardView.swift
@@ -11,9 +11,17 @@ struct CardView: View {
     
     var card: Card
     
+    @State private var fadeIn: Bool = false
+    @State private var moveDownward: Bool = false
+    @State private var moveUpward: Bool = false
+    
+    var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
+    // other values are soft, light, medium, rigid and we cn even have custom ones
+    
     var body: some View {
         ZStack{
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
             
             VStack{
                 Text(card.title)
@@ -27,10 +35,11 @@ struct CardView: View {
                     .foregroundColor(Color.white)
                     .italic()
             }
-            .offset(y: -218)
+            .offset(y: moveDownward ? -218 : -300)
             
             Button {
                 playSound(sound: "sound-chime", type: "mp3")
+                self.hapticImpact.impactOccurred()
             } label: {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -48,13 +57,22 @@ struct CardView: View {
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
-            .offset(y: 210)
+            .offset(y: moveUpward ? 210 : 300)
         }
         .frame(width: 335, height: 545)
         //        .background(.pink)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear() {
+            withAnimation(.linear(duration: 1.2)) {
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)) {
+                self.moveDownward.toggle()
+                self.moveUpward.toggle()
+            }
+        }
     }
 }
 
